df1583c7770fd2fe99aae1242a735294938f68e7
Rokas <rookas.rudzenskas@gmail.com>
2022-09-22 17:03:46 +0300

    Rokas

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -7,31 +7,65 @@ pragma solidity ^0.8.15;
 contract Transactions {
     uint256 transactionCount;
 
-    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp);
 
-    struct TransferStruct {
-        address sender;
-        address receiver;
-        uint amount;
-        string message;
-        uint256 timestamp;
 
-    }
 
-    TransferStruct[] transactions;
 
-    function addToBlockchain(address payable receiver, uint amount, string memory message) public {
-        transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp));
 
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp);
-    }
 
-    function getAllTransactions() public view returns (TransferStruct[] memory) {
-        return transactions;
-    }
 
-    function getTransactionCount() public view returns (uint256) {
-        return transactionCount;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    uint256 transactionCount;
+//
+//    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp);
+//
+//    struct TransferStruct {
+//        address sender;
+//        address receiver;
+//        uint amount;
+//        string message;
+//        uint256 timestamp;
+//
+//    }
+//
+//    TransferStruct[] transactions;
+//
+//    function addToBlockchain(address payable receiver, uint amount, string memory message) public {
+//        transactionCount += 1;
+//        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp));
+//
+//        emit Transfer(msg.sender, receiver, amount, message, block.timestamp);
+//    }
+//
+//    function getAllTransactions() public view returns (TransferStruct[] memory) {
+//        return transactions;
+//    }
+//
+//    function getTransactionCount() public view returns (uint256) {
+//        return transactionCount;
+//    }
 }
